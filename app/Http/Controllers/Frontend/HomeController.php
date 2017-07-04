e253c36a05e97933a5c7ec2d211e6317c3d1c28c
phongvt3110 <vtphong651043@gmail.com>
2017-07-05 00:15:37 +0700

    update code

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -7,7 +7,14 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     public function index(){
-        $data = ['users' => ['id'=>'1','name'=>'Phongvt','email' => 'vtphong651043@gmail.com']];
+        $data = [
+            'users' => [
+                'id'=>'1',
+                'name'=>'Phongvt',
+                'email' => 'vtphong651043@gmail.com',
+                'phone' => '0983397580'
+            ]
+        ];
         return view('frontend/index',['data' => isset($data) ? $data : null]);
     }
 }
