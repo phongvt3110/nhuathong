6410389cac21ef44c22dbb70437a697e74da09e7
ex-phongvt <ex.phongvt@nextbeat.net>
2017-07-04 10:18:49 +0700

    update code from mac

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'nhuathong'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'monkey3110'),
+            'password' => env('DB_PASSWORD', 'livet'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
