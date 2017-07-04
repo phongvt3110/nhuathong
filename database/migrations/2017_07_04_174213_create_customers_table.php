ed017d4311e35e9efd3edf67f7d7a166e9d4a70f
phongvt3110 <vtphong651043@gmail.com>
2017-07-05 01:28:35 +0700

    update code

--- database/migrations/2017_07_04_174213_create_customers_table.php
+++ database/migrations/2017_07_04_174213_create_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCustomersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name',255);
+            $table->string('email',255);
+            $table->string('phone',15);
+            $table->string('address',255);
+            $table->timestamp('registered_at');
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 }
